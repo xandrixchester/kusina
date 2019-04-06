6df309f654c7f8625a55b83f2f6eb44a307dbbe6
xandrix <xandrixecklor@hotmaill.com>
2019-04-06 16:04:50 +0000

    Update Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,6 +21,17 @@ class PlacesController < ApplicationController
  		@place = Place.find(params[:id])
  	end
 
+
+ 	def edit
+ 		@place = Place.find(params[:id])
+ 	end 
+
+ 	def update
+ 		@place = Place.find(params[:id])
+  		@place.update_attributes(place_params)
+  		redirect_to root_path
+ 	end 
+ 
   private
 
   	def place_params
