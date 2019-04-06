b5bdd2d3c4ef97398ada52a9dd16ea536c6bb12b
xandrix <xandrixecklor@hotmaill.com>
2019-04-06 16:24:46 +0000

    only users can delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -44,6 +44,10 @@ class PlacesController < ApplicationController
 
  	def destroy
  		@place = Place.find(params[:id])
+ 		if @place.user != current_user
+    		return render plain: 'Not Allowed', status: :forbidden
+  		end
+ 		
   		@place.destroy
   		redirect_to root_path
  	end
